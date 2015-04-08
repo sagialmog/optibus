4abb7a39a934dcf8e53812199520cce72e61c341
Sagi Almog <sagialmog@gmail.com>
2015-04-08 15:37:30 +0300

    server port change

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/api/optibus/' , function(req ,res){
 });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
